1a766d8d1dec5b57d57f7c4bbae141282ef4648c
redefik <fe.viglietta@gmail.com>
2020-03-18 16:46:42 +0100

    Added caching policy

--- start-containers.sh
+++ start-containers.sh
@@ -9,8 +9,8 @@ docker run -i -t -d -p 27017:27017 --name mongo_server --network=mongo_redis_net
 
 # Launch a redis_server container listening on port 6379 attached to
 # mongo_redis_net network
-docker run -i -t -d -p 6379:6379 --name redis_server --network=mongo_redis_net sickp/alpine-redis
+docker run -i -t -d -p 6379:6379 --name redis_server --network=mongo_redis_net my_redis_cache
 
 # Launch a container executing the Python web service listening on 0.0.0.0:80
 # The container is attached to the mongo_redis_net network
-docker run -i -t -p 80:80 --name python_app --network=mongo_redis_net my_mongo_redis
+docker run -i -t -d -p 80:80 --name python_app --network=mongo_redis_net my_mongo_redis
